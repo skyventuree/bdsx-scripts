7fccbed5d117ef5f6007608cf71c3c26d7a93332
Rosetta Emerson <skyventuree@users.noreply.github.com>
2021-03-06 16:21:28 +0700

    add callbac

--- tellraw.ts
+++ tellraw.ts
@@ -5,5 +5,7 @@ const system = server.registerSystem(0, 0);
 
 export function tellraw (playerName: string, message: string, optionalJSON?: string) {
     optionalJSON = "," + optionalJSON || "";
-    system.executeCommand(`tellraw {playerName} \{"rawtext":[\{"text":"{message}"\}]{optionalJSON}\}`)
+    system.executeCommand(`tellraw {playerName} \{"rawtext":[\{"text":"{message}"\}]{optionalJSON}\}`, res => {
+        console.log(res);
+    })
 }
